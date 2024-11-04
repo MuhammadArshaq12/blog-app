7997425d31c818ebd8467fe0e7a987ee1af5471e
Arshaq Wajid <arshaqwajid@gmail.com>
2024-11-05 02:12:58 +0500

    Init

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -206,7 +206,7 @@ const Register = () => {
               country={'us'}
               value={formData.phoneNumber}
               onChange={handlePhoneChange}
-              inputClass="block w-300 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              inputClass="block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               placeholder="+1 234 567 890"
             />
             {errors.phoneNumber && <p className="mt-1 text-xs text-red-600">{errors.phoneNumber}</p>}
